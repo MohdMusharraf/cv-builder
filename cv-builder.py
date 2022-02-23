285cc30f9b62dc5941ea4908223ae6e0acbda0ac
MohdMusharraf <codejam9888@gmail.com>
2022-02-23 10:11:00 +0530

    three input fields added

--- cv-builder.py
+++ cv-builder.py
@@ -2,4 +2,10 @@ from docx import Document
 
 document = Document()
 
+name = input("What is your name : ")
+phone = input("What is your phone no. ")
+email = input("What is your email : ")
+
+document.add_paragraph(name + " | " + phone + " | " + email)
+
 document.save("my-cv.docx")
