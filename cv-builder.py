a29a77dc4387a4953c1898f918d1a9e7f0574123
MohdMusharraf <codejam9888@gmail.com>
2022-02-25 23:35:18 +0530

    about me section is added

--- cv-builder.py
+++ cv-builder.py
@@ -3,16 +3,22 @@ from docx.shared import Inches
 
 doc = Document()
 
+#profile picture
 doc.add_picture(
     "musharraf.png",
     width=Inches(2.0)
-
 )
 
+# name, phone no. and email details
 name = input("What is your name : ")
 phone = input("What is your phone no. ")
 email = input("What is your email : ")
 
 doc.add_paragraph(name + " | " + phone + " | " + email)
+
+# About me
+doc.add_heading("About Me")
+about_me=input('Tell me about yourself ? ')
+doc.add_paragraph(about_me)
 
 doc.save("my-cv.docx")
